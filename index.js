dfe79f2d6f7fae84a814d916faeeca079778182d
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2018-08-06 21:40:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const BuiltIns = [Symbol, Boolean, Number, String, Object, Array, Set, Map, WeakMap, WeakSet,
+                  Uint8Array, Uint16Array, Uint32Array, Float32Array, Float64Array,
+                  Int8Array, Int16Array, Int32Array, 
+                  Uint8ClampedArray];
+const typeCache = new WeakMap();
 
+mapBuiltins();
 
 Object.assign(T, {check, verify, def, defCollection, option, enum, guard, errors});
 
@@ -32,5 +38,9 @@ function guard(typeName, instance) {
 
 function errors(typeName, instance) {
 
+}
+
+function mapBuiltins() {
+  
 }
 
