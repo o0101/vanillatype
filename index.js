9f3c1110b65c7e9d9cd53124de51fe7f015c1639
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2018-08-06 21:36:41 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+Object.assign(T, {check, verify, def, defCollection, option, enum, guard, errors});
+
+export default function T(parts, ...vals) {
+  parts = [...parts];
+  vals = [...vals];
+  const typeName = parts[0];
+}
+
+function check(typeName, instance) {
+
+}
+
+function verify(...args) { return check(..args); }
+
+function defCollection(name, {container, member}) {
+
+}
+
+function def(name, spec) {
+
+}
+
+function enum(...types) {
+
+}
+
+function guard(typeName, instance) {
+
+}
+
+function errors(typeName, instance) {
+
+}
+
