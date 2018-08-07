7946adb49d46284bd42c1acb495a5a7e29c4b614
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-08-08 02:38:21 +0800

    First type checks are passing

--- jtype.js
+++ jtype.js
@@ -6,7 +6,9 @@
 
   mapBuiltins();
 
-  Object.assign(T, {check, verify, def, defCollection, option, or, guard, errors});
+  Object.assign(T, {check, verify, validate, def, defCollection, option, or, guard, errors});
+
+  Object.assign(self, {typeCache});
 
   export function T(parts, ...vals) {
     parts = [...parts];
@@ -16,8 +18,72 @@
     return new Type(typeName);
   }
 
-  function check(typeName, instance) {
+  function validate(type, instance) {
+    let typeName = type;
+    if (type instanceof Type) {
+      typeName = type.name;
+    }
 
+    const {spec,kind,verify} = typeCache.get(typeName);
+    if ( ! kind ) throw { error: `No such type defined: ${typeName}` };
+
+    const bigErrors = [];
+
+    switch(kind) {
+      case "def":
+        let allValid = true;
+        if ( !! spec ) {
+          const keyPaths = Object.keys(spec);
+          allValid = keyPaths.every(kp => {
+            const {resolved, errors:lookupErrors} = lookup(instance,kp);
+            bigErrors.push(...lookupErrors);
+            if ( lookupErrors.length ) return false;
+            const {valid, errors: validationErrors} = validate(spec[kp], resolved);
+            bigErrors.push(...validationErrors);
+            return valid;
+          });
+        }
+        let verified = true;
+        if ( !!verify ) {
+          try {
+            verified = verify(instance);
+          } catch(e) {
+            bigErrors.push(e);
+          }
+        }
+        return {valid: allValid && verified, errors: bigErrors}
+      default: {
+        throw {error: `Checking for type kind ${kind} is not yet implemented.`}
+      }
+    }
+  }
+
+  function check(...args) {
+    return validate(...args).valid;
+  }
+
+  function lookup(obj, keyPath) {
+    if ( !obj ) throw {error:`Lookup requires a non-unset object.`};
+
+    if ( !keyPath ) throw {error: `keyPath must not be empty`};
+
+    const keys = keyPath.split(/\./g);
+    const pathComplete = [];
+    const errors = [];
+
+    let resolved = obj;
+
+    while(keys.length) {
+      const nextKey = keys.shift();
+      resolved = resolved[nextKey];
+      pathComplete.push(nextKey);
+      if ( keys.length && resolved == null || resolved == undefined ) {
+        errors.push( { error: `Lookup on key path ${keyPath} failed at ${pathComplete.join('.')}
+          when null or undefined was found.` });
+        break;
+      }
+    }
+    return {resolved,errors};
   }
 
   function option(type) {
@@ -28,8 +94,8 @@
   function defCollection(name, {container, member}) {
     if ( !name ) throw {error:`Type must be named.`}; 
     if ( !container || !member ) throw {error:`Type must be specified.`};
-    spec.kind = 'defCollection';
-    const spec = {container, member};
+    const kind = 'defCollection';
+    const spec = {kind, spec: { container, member}};
     typeCache.set(name, spec);
     return new Type(name);
   }
@@ -44,11 +110,11 @@
     return `${this.name} Type`;
   };
 
-  function def(name, spec) {
+  function def(name, spec, {verify} = {}) {
     if ( !name ) throw {error:`Type must be named.`}; 
-    if ( !spec ) throw {error:`Type must be specified.`};
-    spec.kind = 'def';
-    typeCache.set(name, spec);
+
+    const kind = 'def';
+    typeCache.set(name, {spec,kind,verify});
     return new Type(name);
   }
 
@@ -60,11 +126,11 @@
     if ( ! verify(typeName, instance) ) throw {error: `Type ${typeName} requested, but item is not of that type.`};
   }
 
-  function errors(typeName, instance) {
-
+  function errors(...args) {
+    return validate(...args).errors;
   }
 
   function mapBuiltins() {
-    BuiltIns.forEach(t => def(t.name, {verify: i => i.constructor.name === name}));  
+    BuiltIns.forEach(t => def(t.name, null, {verify: i => i.constructor.name === t.name}));  
   }
 
